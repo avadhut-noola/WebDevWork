74c4a4c626cd14c56b5a70c2dfe5b5c942711a30
Avadhut Noola <avdhutnula@gmail.com>
2025-01-10 17:04:12 +0530

    BMI - Tells you're Under, Over or Normal Weight

--- JavaScriptEx/bmiCalculator.js
+++ JavaScriptEx/bmiCalculator.js
@@ -4,7 +4,18 @@ Try to code in less lines*/
 
 function bmiCalculator(weight, height) {
     var bmi = Math.round(weight / Math.pow(height,2));
-    return bmi;
+
+    //Updated bmi calculator with added: Message as per different bmi.
+    if(bmi <18.5) {
+        var interpretation = "Your BMI "+ bmi+", so you are underweight.";
+    } 
+    else if(bmi >= 18.5 && bmi <=24.9) {
+        interpretation = "Your BMI is "+ bmi+", so you have a normal weight.";
+    } else {
+        interpretation = "Your BMI is "+ bmi+", so you are overweight.";
+    }
+
+    return interpretation;
 }
 
 weight = prompt("Enter weight in kilograms: ")
