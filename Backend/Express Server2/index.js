c1011bb8dc6f412f0f6ce936a0ad3ed98b0cd736
Avadhut Noola <avdhutnula@gmail.com>
2025-01-20 21:29:47 +0530

    Understanding ENDPOINTS of server

--- Backend/Express Server2/index.js
+++ Backend/Express Server2/index.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
     res.send("Hello World, My First Express Server Application!")
 })
 
+// Understanding ENDPOINTS
+app.get("/about", (req, res) => {
+    res.send("Hello World, You're on the <em>about</em> ENDPOINT of Express server!")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Hello World, You're on the <em>contact</em> ENDPOINT of Express server!")
+})
+
+//Starting the server with port 3000
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}.`);
 })
