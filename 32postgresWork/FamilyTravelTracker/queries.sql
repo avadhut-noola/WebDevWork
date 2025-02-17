24b20a2d4041c19cf7c7885b3777070d9b8980a7
Avadhut Noola <avdhutnula@gmail.com>
2025-02-17 22:43:10 +0530

    Form Many to Many relationship

--- 32postgresWork/FamilyTravelTracker/queries.sql
+++ 32postgresWork/FamilyTravelTracker/queries.sql
@@ -45,3 +45,38 @@ SELECT student.id, first_name, last_name, mark
 FROM student
 JOIN homework_submission
 ON student.id = student_id
+
+-- Many to Many --
+CREATE TABLE class (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(45)
+);
+
+CREATE TABLE enrollment (
+  student_id INTEGER REFERENCES student(id),
+  class_id INTEGER REFERENCES class(id),
+  PRIMARY KEY (student_id, class_id)
+);
+
+-- Data --
+INSERT INTO student (first_name, last_name)
+VALUES ('Someone', 'New');
+
+INSERT INTO class (title)
+VALUES ('English Literature'), ('Maths'), ('Physics');
+
+INSERT INTO enrollment (student_id, class_id ) VALUES (1, 1), (1, 2);
+INSERT INTO enrollment (student_id ,class_id) VALUES (2, 2), (2, 3);
+
+-- Join to see Many to Many relation--
+SELECT *
+FROM enrollment 
+JOIN student ON student.id = enrollment.student_id
+JOIN class ON class.id = enrollment.class_id;
+
+SELECT student.id AS id, first_name, last_name, title
+FROM enrollment 
+JOIN student ON student.id = enrollment.student_id
+JOIN class ON class.id = enrollment.class_id;
+
+
