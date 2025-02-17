dee6bcfb2ed85353c8133d3b77ee03f5a87ca10f
Avadhut Noola <avdhutnula@gmail.com>
2025-02-17 20:59:01 +0530

    Many to One relationship

--- 32postgresWork/FamilyTravelTracker/queries.sql
+++ 32postgresWork/FamilyTravelTracker/queries.sql
@@ -23,4 +23,25 @@ FROM student
 JOIN contact_detail
 ON student.id = contact_detail.id
 
+-- Many to One Relationship --
+CREATE TABLE homework_submission (
+  id SERIAL PRIMARY KEY,
+  mark INTEGER,
+  student_id INTEGER REFERENCES student(id)
+);
 
+-- Sample Data --
+INSERT INTO homework_submission (mark, student_id)
+VALUES (98, 1), (87, 1), (88, 1)
+
+-- Join --
+SELECT *
+FROM student
+JOIN homework_submission
+ON student.id = student_id
+
+-- Joining with particular columns
+SELECT student.id, first_name, last_name, mark
+FROM student
+JOIN homework_submission
+ON student.id = student_id
