a3c694b32c399376c8a07228e3ff04b3a16ab845
Avadhut Noola <avdhutnula@gmail.com>
2025-02-28 23:25:48 +0530

    Logout route

--- 33AuthenticationLevelWise/index2.js
+++ 33AuthenticationLevelWise/index2.js
@@ -102,6 +102,15 @@ app.post(
     })
 );
 
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+  });
+
 passport.use(
     new Strategy(async function verify(username, password, cb){
         try {
