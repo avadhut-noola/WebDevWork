ddd89cc31c8082e9cf9694664ecce07338b69964
Avadhut Noola <avdhutnula@gmail.com>
2025-03-03 19:16:13 +0530

    Create React components in App

---
+++ my-react-app/src/list.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List () {
+    return <ul>
+        <li>Development is easy</li>
+        <li>With React Integration</li>
+        <li>It increases the overall pace of development</li>
+    </ul>
+}
+
+export default List;
