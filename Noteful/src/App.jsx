34d29e1f214217b51c365f5ce0bb811b180abfef
Avadhut Noola <avdhutnula@gmail.com>
2025-03-04 18:32:33 +0530

    Noteful- base files

---
+++ Noteful/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Heading from './Heading'
+import Note from './Note'
+import Footer from './footer'
+
+function App() {
+
+  return (
+    <>
+        <Heading />
+        <Note />
+        <Footer />
+    </>
+  )
+}
+
+export default App
