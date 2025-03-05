c69c2d2bd0bfa4282ee239ef92dc121c44e2b327
Avadhut Noola <avdhutnula@gmail.com>
2025-03-05 21:23:03 +0530

    Base App and styles for contact cards

---
+++ 35MyContacts_ReactProps/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import Card from './Card'
+import contacts from './contacts'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="heading">My Inspirations</h1>
+
+        <Card
+          name = {contacts[0].name}
+          imgURL = {contacts[0].imgURL}
+          tel = {contacts[0].tel}
+          email = {contacts[0].email}
+        />
+
+        <Card
+          name = {contacts[1].name}
+          imgURL = {contacts[1].imgURL}
+          tel = {contacts[1].tel}
+          email = {contacts[1].email}
+        />
+
+        <Card
+          name = {contacts[2].name}
+          imgURL = {contacts[2].imgURL}
+          tel = {contacts[2].tel}
+          email = {contacts[2].email}
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
