3e35749f4890cebe52c97a6fcfb0a83202774845
Avadhut Noola <avdhutnula@gmail.com>
2025-03-05 21:24:08 +0530

    Create contacts object

---
+++ 35MyContacts_ReactProps/src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name : "Chris Gardner",
+    imgURL :
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/ChrisGardnerSept2011.jpg/220px-ChrisGardnerSept2011.jpg",
+    tel : "+123 500 100",
+    email : "chrisfighter@gmail.com"
+  },
+  {
+    name : "Aron Ralston",
+    imgURL :
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Aron_Ralston_on_Capitol_Peak_Winter_2003.JPG/220px-Aron_Ralston_on_Capitol_Peak_Winter_2003.JPG",
+    tel : "+123 003 404",
+    email : "arontravels@gmail.com"
+  },
+  {
+    name : "Tom Hanks",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Kevin_Kline_%26_Tom_Hanks_%28252967475%29_%28cropped%29.jpg/170px-Kevin_Kline_%26_Tom_Hanks_%28252967475%29_%28cropped%29.jpg",
+    tel : "+123 200 500",
+    email : "tomactswell@gmail.com"
+  }
+];
+
+export default contacts
