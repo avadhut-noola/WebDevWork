6d805f555d53ad39efdced13e8b230f167cb78ee
Avadhut Noola <avdhutnula@gmail.com>
2025-03-18 17:52:20 +0530

    Render notes in App

--- Noteful/src/components/App.jsx
+++ Noteful/src/components/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 import Heading from './Heading'
 import Note from './Note'
 import Footer from './footer'
+import notes from './notes'
 
 function App() {
 
   return (
     <>
         <Heading />
-        <Note />
+        {/* For each note in JSON file */}
+        {notes.map( note => <Note
+            key = {note.key}
+            title = {note.title}
+            content = {note.content}
+          />
+        )}
         <Footer />
     </>
   )
