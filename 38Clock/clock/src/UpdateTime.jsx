848542c21bd082b0dfd3740643cb80db35247927
Avadhut Noola <avdhutnula@gmail.com>
2025-03-21 12:07:29 +0530

    Use useEffect to update time every second

--- 38Clock/clock/src/UpdateTime.jsx
+++ 38Clock/clock/src/UpdateTime.jsx
@@ -1,26 +1,30 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const UpdateTime = () => {
-    const [time, setTime] = useState("");
+  const [time, setTime] = useState("");
 
-    const getTime = () => {
-        const currentTime = new Date().toLocaleTimeString([], {
+  useEffect(() => {
+    
+    // Set up an interval to update the time every second
+    const intervalId = setInterval(() => {
+      const currentTime = new Date().toLocaleTimeString([], {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
         hour12: false,
-        });
-        setTime(currentTime);
-    }
+      });
+      setTime(currentTime);
+    }, 1000);
 
-    return (
-        <div>
-        <h1>{time}</h1>
+    // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
-        <button onClick={getTime}>Get Time</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{time}</h1>
+    </div>
+  );
 };
 
 export default UpdateTime;
