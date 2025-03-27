0bfcec018e64b0c2db953f5a02766ab7f74da76f
Avadhut Noola <avdhutnula@gmail.com>
2025-03-27 12:34:32 +0530

    Test spread operator

--- 40ComplexState/src/index.jsx
+++ 40ComplexState/src/index.jsx
@@ -3,3 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// Using spread operator
+const fastestCars = ["Bugatti Veyron", "Devel Sixteen", "Hennessey Venom F5", "McLaren Speedtail"];
+const cars = ["Maybach Landaulet", "Pagani Zonda C12 F", "Lamborghini Reventon", ...fastestCars ];
+console.log(cars);
