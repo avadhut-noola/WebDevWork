92eea5b8ebbcb6b3ee9881b512fe2eb34d57a143
Avadhut Noola <avdhutnula@gmail.com>
2025-03-27 12:42:29 +0530

    Use spread operator to prevent nested object creation

--- 40ComplexState/src/index.jsx
+++ 40ComplexState/src/index.jsx
@@ -4,7 +4,24 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// Using spread operator
+// Using spread operator with arrays
 const fastestCars = ["Bugatti Veyron", "Devel Sixteen", "Hennessey Venom F5", "McLaren Speedtail"];
 const cars = ["Maybach Landaulet", "Pagani Zonda C12 F", "Lamborghini Reventon", ...fastestCars ];
 console.log(cars);
+
+// Using spread operator with objects
+
+const fullName = {
+    firstName: "Avadhut",
+    lastName: "Noola"
+};
+
+// if you directly use object inside another object
+// It will be nested object instead use the spread operator like this:
+const user = {
+    ...fullName,
+    id: 1,
+    email: "avadhutnoola@gmail.com"
+};
+
+console.log(user);
